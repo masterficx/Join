021f5bb0ada9cd241519190d7bdddd37e896413b
Marcel Zilkowski <info@internetaces.com>
2023-07-22 11:19:59 +0200

    fixed html include function

--- js/include.js
+++ js/include.js
@@ -1,13 +1,21 @@
+async function includeTemplates() {
+    await includeHTML();
+    const userinitials = await currentuser.split(' ').map(userinitials => userinitials[0]).join('');//.split(' ') erstellt ein Array mit den durch ' ' getrennten Wörtern .join('') entfernt die Kommas.
+    let currentUserInitials = await document.getElementById('myaccount');
+    currentUserInitials.innerHTML = await `${userinitials}`;
+}
+
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
-    const element = includeElements[i];
-    file = element.getAttribute("w3-include-html"); // 
-    let resp = await fetch(file);
-    if (resp.ok) {
-    element.innerHTML = await resp.text();
-    } else {
-    element.innerHTML = 'Page not found';
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
     }
-    }
-    }
+}
+
