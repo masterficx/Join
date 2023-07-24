d18feac7f8151ca8b90523b3938a4e4e040b40a9
Filip Todoroski <nabrzinka@gmail.com>
2023-07-25 00:55:34 +0200

    updated functionality on the contacts list

--- js/contacts.js
+++ js/contacts.js
@@ -37,6 +37,8 @@ let Contacts = [
     }
 ]
 
+let nameTagsColors = ['#FF7A00', '#9327FF', '#29ABE2', '#FC71FF', '#02CF2F', '#AF1616','#FF7A00', '#9327FF', '#29ABE2', '#FC71FF', '#02CF2F', '#AF1616','#FF7A00', '#9327FF', '#29ABE2', '#FC71FF', '#02CF2F', '#AF1616'];
+
 let firstLetters = [];
 function getFirstLetters() {
     let allFirstLetters = [];
@@ -49,7 +51,6 @@ function getFirstLetters() {
         return allFirstLetters.indexOf(element) === index;
     });
 
-    console.log(uniqueChars);
     firstLetters.push(uniqueChars);
 
 }
@@ -58,16 +59,42 @@ function renderContactsList() {
     let contactsList = document.getElementById('contacts_list');
 
     for (let i = 0; i < Contacts.length; i++) {
-        const element = Contacts[i];
-        const firstLetter = element['firstName'].charAt(0);
-        const firstTwoLetters = element['firstName'].charAt(0) + element['lastName'].charAt(0);
-        const letterTab = document.getElementById(`letter_${firstLetter}`)
-        if (letterTab) {
-            return letterTab
+        let element = Contacts[i];
+        let firstLetter = element['firstName'].charAt(0);
+        let firstTwoLetters = element['firstName'].charAt(0) + element['lastName'].charAt(0);
+        let letterTab = document.getElementById(`letter_${firstLetter}`);
+        if (!letterTab) {
+            contactsList.innerHTML +=
+            `   <div class="frame-112" id="letter_${firstLetter}">
+                    <p>${firstLetter}</p>
+                </div> 
+                
+                <div class="frame-119">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="354" height="2" viewBox="0 0 354 2" fill="none">
+                    <path d="M1 1H353" stroke="#D1D1D1" stroke-linecap="round"/>
+                    </svg>
+                </div>
+                <div class="frame_112"> 
+                 
+            </div>
+            <div class="contact_name"> 
+                <div class="frame_79">
+                <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" fill="none">
+                <circle cx="21" cy="21" r="20.5" fill="${nameTagsColors[i]}" stroke="white"/>
+                </svg>
+                <p>${firstTwoLetters}</p>
+            </div>
+            <div class="frame_81">
+                <span>
+                    ${element['firstName']} ${element['lastName']}
+                </span>
+                
+                <a href = "mailto: ${element['email']}">${element['email']}</a>   
+            </div>
+        </div>
+                `
         } else {
-            contactsList.innerHTML += ` `
-        }
-        contactsList.innerHTML += `
+            contactsList.innerHTML += `
             <div class="frame_112"> 
                  
             </div>
@@ -86,6 +113,8 @@ function renderContactsList() {
                 <a href = "mailto: ${element['email']}">${element['email']}</a>   
             </div>
         </div>`
+        }
+
 
     }
 }
