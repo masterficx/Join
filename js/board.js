7c42c5ad67ba1e60b612cb256b70cd1b0fce8b6b
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-07-26 10:13:35 +0200

    board function filterCards

--- js/board.js
+++ js/board.js
@@ -6,15 +6,25 @@ let cards = [
         "progress": "0",
         "assignedUser": [" Paul", " Rita"],
         "prio": "High",
+        "dueDate": "2022-08-14",
+        "subtasks": [
+            { name: "Test Subtask 1", status: "AwaitingFeedback"},
+            { name: "Test Subtask 2", status: "InProgress"}
+        ],
         "listType": "ToDo"
     },
     {
         "category": "Backoffice",
         "title": "Test Title 2",
-        "description": 'Test Card 3',
+        "description": 'Test Card 3 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod temporinvidunt utlabore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duodolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sitamet.',
         "progress": "1",
         "assignedUser": ["Sara", " Mike"],
         "prio": "Low",
+        "dueDate": "2022-08-14",
+        "subtasks": [
+            { name: "Test Subtask 1", status: "AwaitingFeedback"},
+            { name: "Test Subtask 2", status: "InProgress"}
+        ],
         "listType": "InProgress"
     },
     {
@@ -24,6 +34,11 @@ let cards = [
         "progress": "2",
         "assignedUser": ["Klaus", " Paul", " Rita"],
         "prio": "Mid",
+        "dueDate": "2022-08-14",
+        "subtasks": [
+            { name: "Test Subtask 1", status: "AwaitingFeedback"},
+            { name: "Test Subtask 2", status: "InProgress"}
+        ],
         "listType": "AwaitingFeedback"
     },
     {
@@ -31,8 +46,13 @@ let cards = [
         "title": "Test Title 4",
         "description": 'Test Card 4',
         "progress": "1",
-        "assignedUser": ["Sara", " Paul", " Rita"],
+        "assignedUser": ["Klaus", " Rita"],
         "prio": "High",
+        "dueDate": "2022-08-14",
+        "subtasks": [
+            { name: "Test Subtask 1", status: "AwaitingFeedback"},
+            { name: "Test Subtask 2", status: "InProgress"}
+        ],
         "listType": "AwaitingFeedback"
     }
 ]
@@ -78,3 +98,32 @@ function clearBoardCards() {
     document.getElementById('cardBoardAwaitingfeedback').innerHTML = '';
     document.getElementById('cardBoardDone').innerHTML = '';
 }
+
+
+function openAddTask() {
+    document.getElementById('overlay').classList.remove('d-none');
+    document.getElementById('mainBoard').classList.add('d-none');
+    renderAddTask();
+}
+
+
+function renderAddTask() {
+    document.getElementById('CardContainer').innerHTML =`
+    <div class="includeTaskForm" w3-include-html="templates/task_form.html">
+    `;
+}
+
+function filterCards() {
+    const query = document.getElementById("inputSearchBoard").value.toLowerCase();
+    const cards = document.querySelectorAll(".cardBoard");
+  
+    cards.forEach((card) => {
+      const title = card.querySelector(".cardBoardInsideTitle").innerHTML.toLowerCase();
+      const description = card.querySelector(".cardBoardInsideDescription").innerHTML.toLowerCase();
+      if (title.includes(query) || description.includes(query)) {
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  }
