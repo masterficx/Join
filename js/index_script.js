1d942aa7c738a5978033fc5bde63ab601e91b4e3
paclem <pascal.moeller@posteo.de>
2023-07-29 19:31:22 +0200

    change comments to english, added resetPasswordForm

--- js/index_script.js
+++ js/index_script.js
@@ -1,10 +1,10 @@
-// Funktion zum Initialisieren der Seite
+// Function to initialize the page
 function init() {
     startAnimation();
 }
 
 
-// Funktion zum Starten der Animation
+// Function to start the animation
 function startAnimation() {
     setTimeout(function animationCallback() {
         let logo = document.getElementById("logo");
@@ -12,21 +12,20 @@ function startAnimation() {
 
         logo.classList.add('imgLogo');
 
-        // Lässt langsam die startBackground div verschwinden
+        // Fades out the startBackground div gradually
         background.style.backgroundColor = "rgba(246, 247, 248, 0%)";
 
-        // Nach der Animation entferne das Bild aus dem DOM
+        // Remove the image from the DOM after the animation
         setTimeout(function removeBackground() {
             if (background && background.parentNode) {
                 background.parentNode.removeChild(background);
             }
-        }, 500); // Warte 0.5 Sekunde (dieselbe Dauer wie die Animation) bevor das Bild entfernt wird
-
-    }, 500); // Warte 0.5 Sekunden bevor die Animation startet
+        }, 500); // Wait for 0.5 seconds (same duration as the animation) before removing the image
+    }, 500); // Wait for 0.5 seconds before starting the animation
 }
 
 
-// Funktion zum Umschalten der Passwortsichtbarkeit
+// Function to toggle password visibility
 function togglePasswordVisibility(i) {
     let passwordInput = document.getElementById('passwordInput');
     let passwordImage = document.getElementById('passwordImage');
@@ -58,7 +57,7 @@ function togglePasswordVisibility(i) {
 }
 
 
-// Funktion zum Hinzufügen der Event Listener für das Passwortfeld
+// Function to add event listeners for the password field
 function setupPasswordInputEventListeners() {
     const passwordInputs = document.querySelectorAll('.passwordInput');
 
@@ -89,13 +88,13 @@ function setupPasswordInputEventListeners() {
 }
 
 
-// Funktion zum Hinzufügen der Event Listener nachdem das DOM geladen wurde
+// Function to add event listeners after the DOM has loaded
 function setupEventListenersAfterDOMLoaded() {
     setupPasswordInputEventListeners();
 }
 
 
-// Funktion, die aufgerufen wird, wenn der "Remember Me" Button geklickt wird
+// Function called when the "Remember Me" button is clicked
 function checkBox() {
     let rememberMeImg = document.getElementById('rememberMe');
 
@@ -109,120 +108,41 @@ function checkBox() {
 }
 
 
-
-
-
-// Funktion, die für den Test verwendet wird
+// Function used for testing
 function test() {
-    if (signUp()) {
         console.log('Passt!');
-    }
 }
 
 
-function toggleButtonState() {
-    const myButton = document.getElementById('signUp');
-    if (myButton.disabled) {
-        myButton.removeAttribute('disabled'); // Button aktivieren
-    } else {
-        myButton.setAttribute('disabled', true); // Button deaktivieren
-    }
-}
-
-
-// Funktion zum Rendern des LogIn-Formulars
+// Function to render the LogIn form
 function renderLogIn() {
     let contentbox = document.getElementById('contentbox');
 
-    contentbox.innerHTML = /* html */ `
-
-<form onsubmit="test(); return false;" class="content">
-    <div class="headingContainer">
-    <h1>Log in</h1>
-    </div>
-    <div class="blueSeperator"></div>
-    <div>
-        <div class="inputBox">
-            <div class="inputField">
-                <input required id="emailInput" class="" type="email" placeholder="Email">
-                <img src="./assets/img/logInSignUp/mail.svg" alt="">
-            </div>
-            <div class="inputField">
-                <input required id="passwordInput" class="passwordInput" type="password" placeholder="Password">
-                <img id="passwordImage" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(1)">
-            </div>
-            <div class="rememberMeForgetBox">
-                <div class="checkBoxLogIn">
-                    <div onclick="checkBox()" id="rememberMe" class="uncheckBox"></div>
-                    <span>Remember me</span>
-                </div>
-                <a id="fmp" href="#" onclick="renderForgotPW()"> Forget my password</a>
-            </div>
-</div>
-    </div>
-    <div class="logInButtonBox">
-        <button type="onsubmit" class="logInButton">Log in</button>
-        <button type="button" onclick="test()" class="logInButton guestLogIn">Guest Log in</button>
-    </div>
-</form>
-        `;
+    contentbox.innerHTML = returnLogInHTML();
     setupEventListenersAfterDOMLoaded();
     document.getElementById('headerRight').classList.remove('d-none');
     document.getElementById('footer').classList.remove('d-none');
 }
 
 
-// Funktion zum Rendern des SignUp-Formulars
+// Function to render the SignUp form
 function renderSignUp() {
     let contentbox = document.getElementById('contentbox');
 
-    contentbox.innerHTML = /* html */ `
-
-<form onsubmit="test(); return false;" class="content">
-    <div class="headingContainer">
-    <div onclick="renderLogIn()" class="imgHeadingContainer backArrow"></div>
-    <h1>Sign Up</h1>
-    <div class="imgHeadingContainer"></div>
-    </div>
-    <div class="blueSeperator"></div>
-    <div>
-        <div class="inputBox">
-            <div class="inputField">
-                <input required id="nameInput" class="" type="text" placeholder="Name">
-                <img src="./assets/img/logInSignUp/person.svg" alt="">
-            </div>
-            <div class="inputField">
-                <input required id="emailInput" class="" type="email" placeholder="Email">
-                <img src="./assets/img/logInSignUp/mail.svg" alt="">
-            </div>
-            <div class="inputField">
-                <input required id="passwordInput" class="passwordInput" type="password" placeholder="Password">
-                <img id="passwordImage" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(1)">
-            </div>
-            <div class="inputField">
-                <input required id="passwordInput2" class="passwordInput" type="password" placeholder="Password">
-                <img id="passwordImage2" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(2)">
-            </div>
-            <div id="passwordAlert"></div>
-            <div class="rememberMeForgetBox">
-                <div class="checkBoxSignIn">
-                <input type="checkbox" id="rememberMe" class="uncheckBox" required>
-                    <span>I accept the </span>
-                </div>
-                <a id="fmp" href="#"> Privacy policy</a>
-            </div>
-</div>
-    </div>
-    <div class="logInButtonBox">
-        <button id="signUp" type="onsubmit" class="logInButton">Sign up</button>
-    </div>
-</form>
-        `;
+    contentbox.innerHTML = returnSignUpHTML();
     setupEventListenersAfterDOMLoaded();
     document.getElementById('headerRight').classList.add('d-none');
+    document.getElementById('banner').innerHTML = 'You Signed Up succeccfully';
 }
 
 
+// Function to handle SignUp form submission
+function signUpForm() {
+    signUp();
+}
+
+
+// Function to handle SignUp
 function signUp() {
     let password1 = document.getElementById('passwordInput');
     let password2 = document.getElementById('passwordInput2');
@@ -236,41 +156,180 @@ function signUp() {
         }, 3000);
     } else {
         console.log('Passwörter stimmen überein');
-        return true;
     }
 }
 
 
+// Function to render the Forgot Password form
 function renderForgotPW() {
     let contentbox = document.getElementById('contentbox');
 
-    contentbox.innerHTML = /* html */ `
-
-<form onsubmit="test(); return false;" class="content forgotPW">
-    <div class="headingContainer">
-    <div onclick="renderLogIn()" class="imgHeadingContainer backArrow"></div>
-    <h1>I forgot my password</h1>
-    <div class="imgHeadingContainer"></div>
-    </div>
-    <div class="blueSeperator"></div>
-    <h2>Don't worry! We will send you an email with the instructions to reset your password.</h2>
-    <div>
-        <div class="inputBox">
-            <div class="inputField">
-                <input required id="emailInput" class="" type="email" placeholder="Email">
-                <img src="./assets/img/logInSignUp/mail.svg" alt="">
-            </div>
-</div>
-    </div>
-    <div class="logInButtonBox">
-        <button id="signUp" type="onsubmit" class="sendEmailButton">Send me the email</button>
-    </div>
-</form>
-        `;
+    contentbox.innerHTML = returnForgotPWHTML();
     document.getElementById('headerRight').classList.add('d-none');
     document.getElementById('footer').classList.add('d-none');
 }
 
+// Function to render the Reset Password form
+function renderResetPassword() {
+    let contentbox = document.getElementById('contentbox');
 
-// Event Listener, der init() aufruft, wenn das DOM geladen ist
+    contentbox.innerHTML = returnResetPasswordHTML();
+    document.getElementById('headerRight').classList.add('d-none');
+    document.getElementById('footer').classList.add('d-none');
+}
+
+// Function to show a banner
+function show() {
+    let banner = document.getElementById('banner');
+
+    banner.classList.add("visible");
+    setTimeout(() => {
+        renderSignUp();
+        banner.classList.remove("visible");
+    }, 2000)
+}
+
+
+// Event listener that calls init() when the DOM is loaded
 document.addEventListener('DOMContentLoaded', init);
+
+
+///////////////////////////////////////////////return HTML////////////////////////////////////////////////////////////
+
+
+function returnLogInHTML() {
+    return /* html */ `
+        <form onsubmit="test(); return false;" class="content">
+            <div class="headingContainer">
+                <h1>Log in</h1>
+            </div>
+            <div class="blueSeperator"></div>
+            <div>
+                <div class="inputBox">
+                    <div class="inputField">
+                        <input required id="emailInput" class="" type="email" placeholder="Email">
+                        <img src="./assets/img/logInSignUp/mail.svg" alt="">
+                    </div>
+                    <div class="inputField">
+                        <input required id="passwordInput" class="passwordInput" type="password" placeholder="Password">
+                        <img id="passwordImage" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(1)">
+                    </div>
+                    <div id="passwordAlert"></div>
+                    <div class="rememberMeForgetBox">
+                        <div class="checkBoxLogIn">
+                            <div onclick="checkBox()" id="rememberMe" class="uncheckBox"></div>
+                            <span>Remember me</span>
+                        </div>
+                        <a id="fmp" href="#" onclick="renderForgotPW()"> Forget my password</a>
+                    </div>
+                </div>
+            </div>
+            <div class="logInButtonBox">
+                <button type="onsubmit" class="logInButton">Log in</button>
+                <button type="button" onclick="test()" class="logInButton guestLogIn">Guest Log in</button>
+            </div>
+        </form>
+    `;
+}
+
+
+function returnSignUpHTML() {
+    return /* html */ `
+        <form onsubmit="signUpForm(); return false;" class="content">
+            <div class="headingContainer">
+                <div onclick="renderLogIn()" class="imgHeadingContainer backArrow"></div>
+                <h1>Sign Up</h1>
+                <div class="imgHeadingContainer"></div>
+            </div>
+            <div class="blueSeperator"></div>
+            <div>
+                <div class="inputBox">
+                    <div class="inputField">
+                        <input required id="nameInput" class="" type="text" placeholder="Name">
+                        <img src="./assets/img/logInSignUp/person.svg" alt="">
+                    </div>
+                    <div class="inputField">
+                        <input required id="emailInput" class="" type="email" placeholder="Email">
+                        <img src="./assets/img/logInSignUp/mail.svg" alt="">
+                    </div>
+                    <div class="inputField">
+                        <input required id="passwordInput" class="passwordInput" type="password" placeholder="Password">
+                        <img id="passwordImage" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(1)">
+                    </div>
+                    <div class="inputField">
+                        <input required id="passwordInput2" class="passwordInput" type="password" placeholder="Password">
+                        <img id="passwordImage2" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(2)">
+                    </div>
+                    <div id="passwordAlert"></div>
+                    <div class="rememberMeForgetBox">
+                        <div class="checkBoxSignIn">
+                            <input type="checkbox" id="rememberMe" class="uncheckBox" required>
+                            <span>I accept the </span>
+                        </div>
+                        <a id="fmp" href="#"> Privacy policy</a>
+                    </div>
+                </div>
+            </div>
+            <div class="logInButtonBox">
+                <button id="signUp" type="onsubmit" class="logInButton">Sign up</button>
+            </div>
+        </form>
+    `;
+}
+
+
+function returnForgotPWHTML() {
+    return /* html */ `
+        <form onsubmit="test(); return false;" class="content forgotPW">
+            <div class="headingContainer">
+                <div onclick="renderLogIn()" class="imgHeadingContainer backArrow"></div>
+                <h1>I forgot my password</h1>
+                <div class="imgHeadingContainer"></div>
+            </div>
+            <div class="blueSeperator"></div>
+            <h2>Don't worry! We will send you an email with the instructions to reset your password.</h2>
+            <div>
+                <div class="inputBox">
+                    <div class="inputField">
+                        <input required id="emailInput" class="" type="email" placeholder="Email">
+                        <img src="./assets/img/logInSignUp/mail.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="logInButtonBox">
+                <button id="signUp" type="onsubmit" class="sendEmailButton">Send me the email</button>
+            </div>
+        </form>
+    `;
+}
+
+
+function returnResetPasswordHTML() {
+    return /* html */ `
+        <form onsubmit="test(); return false;" class="content forgotPW">
+            <div class="headingContainer">
+                <div onclick="renderLogIn()" class="imgHeadingContainer backArrow"></div>
+                <h1>Reset your password</h1>
+                <div class="imgHeadingContainer"></div>
+            </div>
+            <div class="blueSeperator"></div>
+            <h2>Change your account password</h2>
+            <div>
+                <div class="inputBox">
+                    <div class="inputField">
+                        <input required id="passwordInput" class="passwordInput" type="password" placeholder="Password">
+                        <img id="passwordImage" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(1)">
+                    </div>
+                    <div class="inputField">
+                        <input required id="passwordInput2" class="passwordInput" type="password" placeholder="Password">
+                        <img id="passwordImage2" class="passwordImage" src="./assets/img/logInSignUp/lock.svg" alt="" onclick="togglePasswordVisibility(2)">
+                    </div>
+                    <div id="passwordAlert"></div>
+                </div>
+            </div>
+            <div class="logInButtonBox">
+                <button id="signUp" type="onsubmit" class="continueButton">Continue</button>
+            </div>
+        </form>
+    `;
+}
