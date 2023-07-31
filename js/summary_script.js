f76785be9c1d81960b02c25aaed866b39031aee5
Marcel Zilkowski <info@internetaces.com>
2023-07-31 23:33:19 +0200

    update counter function

--- js/summary_script.js
+++ js/summary_script.js
@@ -7,6 +7,8 @@ async function init() {
     welcomeMsgTime();
     welcomeMsgUser();
     addClassMobile();
+    summaryTasks();
+    filterCardsPrioHigh();
 }
 
 //Begrüßung zur aktuellen Tageszeit
@@ -64,5 +66,64 @@ function addClassMobile() {
 }
 
 
-// Prüft bei Veränderung die Media Query Conditions
+//Prüft bei Veränderung die Media Query Conditions
   window.addEventListener('resize', addClassMobile);
+
+
+//Count tasks
+function summaryTasks(){
+    //all
+    let allBox = document.getElementById('sum-1');
+    allBox.innerHTML = `${cards.length}`;
+    //progress
+    let progressBox = document.getElementById('sum-2');
+    const countProg = cards.filter(item => item.listType === 'InProgress').length; 
+    progressBox.innerHTML = `${countProg}`;
+    let awaitBox = document.getElementById('sum-3');
+    const countAwait = cards.filter(item => item.listType === 'Awaitingfeedback').length; 
+    awaitBox.innerHTML = `${countAwait}`;
+    let prioBox = document.getElementById('sum-4');
+    const countPrio = cards.filter(item => item.prio === 'High').length; 
+    prioBox.innerHTML = `${countPrio}`;
+    let toDoBox = document.getElementById('sum-5');
+    const countToDo = cards.filter(item => item.listType === 'ToDo').length; 
+    toDoBox.innerHTML = `${countToDo}`;
+    let doneBox = document.getElementById('sum-6');
+    const countDone = cards.filter(item => item.listType === 'Done').length; 
+    doneBox.innerHTML = `${countDone}`;
+}
+
+
+let filteredPrioHigh = [];
+function filterCardsPrioHigh(){
+for(f=0; f<cards.length;f++){
+    if (cards[f].prio == 'High') {
+        filteredPrioHigh.push(cards[f].dueDate);
+}
+};
+sortCardsPrioHigh();
+}
+
+function sortCardsPrioHigh(){
+    filteredPrioHigh.forEach(function (role) {
+        role = role.replace('-','');
+    });
+    console.log(filteredPrioHigh);
+    let upComingDeadline = document.getElementById('upcomingDeadline');
+    upComingDeadline.innerHTML = filteredPrioHigh[0];
+}
+
+
+// Klick im Header auf "My account" zeigt das Menü
+function showMenu(){
+let myAccountMenu = document.getElementById('myaccount');
+let subMenu = document.getElementById('headsubmenu');
+if(subMenu.style.display == "none"){
+subMenu.style = "display: block;";
+myAccountMenu.classList.add('show');
+}
+else{
+    subMenu.style = "display: none;";
+    myAccountMenu.classList.remove('show');   
+};
+}
