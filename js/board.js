4dec89fc88dbb29e13f3369937f86a5962844463
Marcel Zilkowski <info@internetaces.com>
2023-08-12 00:19:57 +0200

    edit subtasks

--- js/board.js
+++ js/board.js
@@ -456,9 +456,93 @@ function editCard(i) {
                                         src="/assets/img/addtask/prio-low-w.svg" alt="" class="active"></button>
                                 </div>`;
     document.getElementById('editCardAssignedTo').value = `${cards[i]['assignedUser']}`;
+    document.getElementById('editCardSubtasks').innerHTML = `
+    <div class="subtask" id="subtask_main2">
+        <h5>Subtasks</h5>
+        <div id="addNewSubtask2" class="subtask-input">
+            <p>Add new subtask</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" onclick="openSubtaskInput2(${i})">
+                <path d="M12.0011 12.0002L12.0018 19.4149M4.58641 12.0008L12.0011 12.0002L4.58641 12.0008ZM19.4159 11.9995L12.0004 11.9995L19.4159 11.9995ZM12.0004 11.9995L12.0005 4.58545L12.0004 11.9995Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </div>
+        <div id="subtasklist"></div>
+        <div class="checkboxes" id="added_subtasks_main">
+        </div>
+    </div>`;
     let editCardSave = document.getElementById('editCardSave');
     editCardSave.innerHTML = `<div onclick='saveEditedCard(${[i]})'>Ok`;
     loadActiveStatePrio(i);
+    loadSubtasksEditform(i);
+}
+
+function loadSubtasksEditform(i){
+    let subtaskMain = document.getElementById('subtasklist');
+    subtaskMain.innerHTML = '';
+for(b=0;b<cards[i]['subtasks'].length; b++){
+    subtaskMain.innerHTML += `<div class="boxes" id="boxes${b}">${cards[i]['subtasks'][b].nameSub}<div class="actionlinks"><a href="#" onclick="editLoadedSubtasks(${i},${b})" class="subTaskEdit">Edit</a><a href="#" onclick="deleteEditedSubtasks(${i},${b})" class="subTaskDel">Del</a></div></div>`;
+}
+}
+
+function editLoadedSubtasks(i,b){
+    let editSubtaskInput = document.getElementById(`subtasklist`);
+    editSubtaskInput.innerHTML = `<input type="text" id='inputEditTask${b}'><div class="editactionlinks" style="display:none;" id="editsubtaskbtn"><a href="#" onclick="cancelEditedSubtask(${i},${b})">Del</a><a href="#" onclick="saveEditedSubtask(${i},${b})">Save</a></div>`;
+    document.getElementById('editsubtaskbtn').style.display = "block";
+    let editSubtaskInputValue = document.getElementById(`inputEditTask${b}`);
+    editSubtaskInputValue.value = `${cards[i]['subtasks'][b].nameSub}`;
+}
+
+function saveEditedSubtask(i,b){
+    document.getElementById('editsubtaskbtn').style.display = "none";
+    let editSubtaskInputValue = document.getElementById(`inputEditTask${b}`);
+    cards[i]['subtasks'][b].nameSub = editSubtaskInputValue.value;
+    loadSubtasksEditform(i);
+}
+
+function openSubtaskInput2(i) {
+    let addSubtaskContainer = document.getElementById('addNewSubtask2');
+    addSubtaskContainer.innerHTML = "";
+    addSubtaskContainer.innerHTML += `
+        
+        <input type="text" placeholder="New subtask" id="added_subtask">
+        <button class="close-category-input-btn" onclick="cancelSubtaskInput2()">${smallXSVG}</button>
+        <svg height="40" width="3">
+            <line x1="2" y1="8" x2="2" y2="32" style="stroke:#d1d1d1;stroke-width:2" />
+        </svg>
+        <button class="add-category-btn" onclick="addSubtask2(${i})">${checkedSmallSVG}</button>
+        `;
+}
+
+function cancelEditedSubtask(i,b){
+    loadSubtasksEditform(i);
+}
+
+function cancelSubtaskInput2() {
+    let addSubtaskContainer = document.getElementById('addNewSubtask2');
+    addSubtaskContainer.innerHTML = `<p>Add new subtask</p>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" onclick="openSubtaskInput2(${i})">
+        <path d="M12.0011 12.0002L12.0018 19.4149M4.58641 12.0008L12.0011 12.0002L4.58641 12.0008ZM19.4159 11.9995L12.0004 11.9995L19.4159 11.9995ZM12.0004 11.9995L12.0005 4.58545L12.0004 11.9995Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+}
+
+function addSubtask2(i) {
+    let subtaskMain = document.getElementById('subtasklist');
+    let addSubtaskContainer = document.getElementById('addNewSubtask2');
+    let addedSubtask = document.getElementById('added_subtask').value; 
+    addSubtaskContainer.innerHTML = `<p>Add new subtask</p>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" onclick="openSubtaskInput2(${i})">
+        <path d="M12.0011 12.0002L12.0018 19.4149M4.58641 12.0008L12.0011 12.0002L4.58641 12.0008ZM19.4159 11.9995L12.0004 11.9995L19.4159 11.9995ZM12.0004 11.9995L12.0005 4.58545L12.0004 11.9995Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+    subtaskMain.innerHTML += `   <div class="boxes" id="boxes${b}">${addedSubtask}<div class="actionlinks"><a href="#" onclick="editLoadedSubtasks(${i},${b})" class="subTaskEdit">Edit</a><a href="#" onclick="deleteEditedSubtasks(${i},${b})" class="subTaskDel">Del</a></div></div>`
+    cards[i]['subtasks'].push({ nameSub: addedSubtask, status: "checked" });
+    addedSubtasks.push(addedSubtask);
+    console.log(addedSubtasks)
+    window.subtasks = addedSubtasks;
+
+}
+
+function deleteEditedSubtasks(i, b){
+    cards[i]['subtasks'].splice(b,1);
+    loadSubtasksEditform(i);
 }
 
 function loadActiveStatePrio(i) {
