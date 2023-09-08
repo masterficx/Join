def935372bcc2ff790324a1c3202c2b604dc7f92
Marcel Zilkowski <info@internetaces.com>
2023-09-08 19:15:02 +0200

    code optimized and added js docs comments

--- js/include.js
+++ js/include.js
@@ -1,33 +1,44 @@
 let currentuser = "Jon Doe";
 
-//Initialen des eingeloggten Nutzers
+/**
+ * Initialize logged in user
+ */
 async function includeTemplates(a) {
     await getContactsFromStorage();
     await includeHTML();
     let currentUserInitials = await document.getElementById('myaccount');
-    if(currentUser !== 1000) {
-    if(currentUser || '0') {  
-        currentUserInitials.innerHTML = await `${Contacts[currentUser].firstLetters}`;
-        } 
+    if (currentUser !== 1000) {
+        if (currentUser || '0') {
+            currentUserInitials.innerHTML = await `${Contacts[currentUser].firstLetters}`;
+        }
     } else {
-            currentUserInitials.innerHTML = await `G`;
-        }
-   
-    //Klasse "active" dem aktuellen Menüpunkt geben
-    let linkElement = [];
-    linkElement = document.getElementById('menu').getElementsByTagName('a');
-    for (i = 0; i < linkElement.length; i++) {
-        if (document.location.pathname === linkElement[i].getAttribute('href')) {
-            linkElement[i].classList.add("active");
-            
-        }
+        currentUserInitials.innerHTML = await `G`;
     }
-    if(a === 1) {
+    addClassActiveMenuItem();
+    if (a === 1) {
         hideHeaderMenu();
     }
     await dateValidation();
 }
 
+
+/**
+ * Adding class to active menu item.
+ */
+function addClassActiveMenuItem() {
+    let linkElement = [];
+    linkElement = document.getElementById('menu').getElementsByTagName('a');
+    for (i = 0; i < linkElement.length; i++) {
+        if (document.location.pathname === linkElement[i].getAttribute('href')) {
+            linkElement[i].classList.add("active");
+        }
+    }
+}
+
+
+/**
+ * Function to include html-templates
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -42,27 +53,38 @@ async function includeHTML() {
     }
 }
 
-// Klick im Header auf "My account" zeigt das Menü
-function showMenu(){
+
+/**
+ * Show "my account - menu" in header
+ */
+function showMenu() {
     let myAccountMenu = document.getElementById('myaccount');
     let subMenu = document.getElementById('headsubmenu');
-    if(subMenu.style.display == "none"){
-    subMenu.style = "display: block;";
-    myAccountMenu.classList.add('show');
+    if (subMenu.style.display == "none") {
+        subMenu.style = "display: block;";
+        myAccountMenu.classList.add('show');
     }
-    else{
+    else {
         subMenu.style = "display: none;";
-        myAccountMenu.classList.remove('show');   
+        myAccountMenu.classList.remove('show');
     };
-    }
+}
 
-function dateValidation(){
-        let dateInput = document.getElementById('date');
-        if(dateInput){
+
+/**
+ * Set minimum date
+ */
+function dateValidation() {
+    let dateInput = document.getElementById('date');
+    if (dateInput) {
         dateInput.setAttribute("min", `${new Date().toISOString().split('T')[0]}`);
     }
-        }
+}
 
+
+/**
+ * Hide header menus by default
+ */
 function hideHeaderMenu() {
     document.getElementById('menu').style.display = "none";
     document.getElementById('headerRight').style.display = "none";
