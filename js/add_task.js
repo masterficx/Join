e1da95252aac85ddb6eaa35469a458048767b20e
Stefan Irrgang <122272798+StefanIrrgang@users.noreply.github.com>
2023-09-09 08:29:58 +0200

    changed alert messages to inline messages in addTask if form not filled

--- js/add_task.js
+++ js/add_task.js
@@ -105,15 +105,15 @@ function checkForInput() {
         return 0;
     }
     if (typeof theChosenCategory == 'undefined') {
-        alert("Please select category");
+        document.getElementById('FieldCategory').style.display = 'block';
         return 0;
     }
     if (typeof addedIds == 'undefined') {
-        alert("Please add contacts");
+        document.getElementById('FieldContact').style.display = 'block';
         return 0;
     }
     if (typeof priority == 'undefined') {
-        alert("Please select priority");
+        document.getElementById('PrioCategory').style.display = 'block';
         return 0;
     }
 
